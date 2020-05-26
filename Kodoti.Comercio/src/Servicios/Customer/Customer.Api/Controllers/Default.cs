529ce0da25af0358912d5392ea2ef6f333317b36
adryfab <adry_fab@hotmail.com>
2020-05-26 09:38:35 -0500

    Creación en blanco de Customer.Api

---
+++ Kodoti.Comercio/src/Servicios/Customer/Customer.Api/Controllers/Default.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class Default : ControllerBase
+    {
+        [HttpGet]
+        public string Inicio()
+        {
+            return "Ejecutando ...";
+        }
+    }
+}
