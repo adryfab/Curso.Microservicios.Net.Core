8876eee8773e8a54ae700888575fca2efa45406e
adryfab <adry_fab@hotmail.com>
2020-05-26 09:44:05 -0500

    Creación del modelo Customer

---
+++ Kodoti.Comercio/src/Servicios/Customer/Customer.Dominio/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer.Dominio
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
